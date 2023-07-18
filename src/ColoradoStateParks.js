7d09f756dbc89d936fa904dc51f8ef8a961e0984
Ann Ndiga <annndiga@gmail.com>
2023-07-18 21:43:36 +0300

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,19 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RockyMountain from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
+  howManyParks (); // =>"42 parks!"
+  RockyMountain.elevation(); // => "9583 ft"
   return <h1>Colorado State Parks!</h1>;
+  
 }
+  
+    console.log(RockyMountain.trees);
+// => 'Aspen and Pine'
+
+RockyMountain.wildlife();
+// => 'Elk, Bighorn Sheep, Moose'
+
+    export default ColoradoStateParks;
